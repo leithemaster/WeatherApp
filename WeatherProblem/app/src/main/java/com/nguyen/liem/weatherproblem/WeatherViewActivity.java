5aaa1b0105d31d5cb1f0d849f777f8eae9265e2a
Liem Nguyen <LTNguyen1@fullsail.edu>
2018-10-17 11:43:47 -0400

    Creations of the project
    This is just the start of the project.
    
    mp:1.0.0

---
+++ WeatherProblem/app/src/main/java/com/nguyen/liem/weatherproblem/WeatherViewActivity.java
@@ -0,0 +1,13 @@
+package com.nguyen.liem.weatherproblem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeatherViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_view);
+    }
+}
